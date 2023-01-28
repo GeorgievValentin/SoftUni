ee6dc22690ab809070a811804fdbb1165c2e99d2
GeorgievValentin <ddarklizard@protonmail.com>
2023-01-28 05:20:07 +0200

    add files

--- Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/test_smtnk.py
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/test_smtnk.py
@@ -1,9 +1,8 @@
-sentence = "Have free hours and love children?"
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
 
-
-def create_bigram(sentence):
-    words = sentence.lower().split()
-    return [(word_1, word_2) for word_1, word_2 in zip(words, words[1:])]
-
-
-print(create_bigram(sentence))
+print(f'{" or ".join(thisdict["colors"])}')
