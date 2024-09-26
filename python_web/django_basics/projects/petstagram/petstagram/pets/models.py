91fcb8b488aee4fbe0855738650b6597a5bf82c5
GeorgievValentin <ddarklizard@protonmail.com>
2024-09-27 00:51:04 +0300

    feat: added pet model

--- python_web/django_basics/projects/petstagram/petstagram/pets/models.py
+++ python_web/django_basics/projects/petstagram/petstagram/pets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length = 30,
+    )
+
+    personal_photo = models.URLField()
+
+    date_of_birth = models.DateField(
+        blank = True,
+        null = True,
+    )
+
+    slug = models.SlugField(
+        unique = True,
+        null = False,
+        blank = True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        super().save(*args, **kwargs)
--- python_web/django_basics/projects/petstagram/petstagram/settings.py
+++ python_web/django_basics/projects/petstagram/petstagram/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = "petstagram.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram_db",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
