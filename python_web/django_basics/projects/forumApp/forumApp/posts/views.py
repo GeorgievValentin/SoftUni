3edcffde30a8a599e9466cb6d03dfa98a9188a73
GeorgievValentin <ddarklizard@protonmail.com>
2024-10-04 00:30:16 +0300

    fix: optimized templates using template inheritance

--- python_web/django_basics/projects/forumApp/forumApp/posts/views.py
+++ python_web/django_basics/projects/forumApp/forumApp/posts/views.py
@@ -8,7 +8,7 @@ def index(request):
         "form": "",
     }
 
-    return render(request, "base.html", context)
+    return render(request, "common/base.html", context)
 
 
 def dashboard(request):
