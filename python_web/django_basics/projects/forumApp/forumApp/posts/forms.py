ee606e611c2ce2960302b2792ccbead14038e609
GeorgievValentin <valggeorgiev@proton.me>
2024-10-08 12:18:46 +0300

    feat: added valodators for PostBaseForm

--- python_web/django_basics/projects/forumApp/forumApp/posts/forms.py
+++ python_web/django_basics/projects/forumApp/forumApp/posts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from forumApp.posts.mixisns import DisableFieldsMixin
 from forumApp.posts.models import PostModel
 
 
@@ -9,12 +12,52 @@ class PostBaseForm(forms.ModelForm):
 
         error_messages = {
             "title": {
-                "max_length": f"The title is too long! Keep it no longer than {PostModel.TITLE_MAX_LENGTH}!"
+                "required": f"The title can't be empty! Write it!",
+                "max_length": f"The title is too long! Keep it no longer than {PostModel.TITLE_MAX_LENGTH}!",
             },
             "author": {
-                "max_length": f"The author's name is too long! Keep it no longer than {PostModel.AUTHOR_MAX_LENGTH}"
+                "required": f"The author can't be empty! Write it!",
+                "max_length": f"The author's name is too long! Keep it no longer than {PostModel.AUTHOR_MAX_LENGTH}",
+            },
+            "content": {
+                "required": "You can't create an empty post!",
             },
         }
+
+    def clean_author(self):  # This validation is better to be set in the model... it is here only for example
+        author = self.cleaned_data.get("author")
+
+        if not author[0].isupper():
+            raise ValidationError("The author's name must start with a Capital letter ")
+
+        return author
+
+    def clean(self):
+        cleaned_data = super().clean()  # !!!!!!
+
+        title = self.cleaned_data.get("title")
+        content = self.cleaned_data.get("content")
+
+        if title and content and title in content:
+            raise ValidationError("You can't use the title in the content")
+
+    def save(self, commit = True):
+        post = super().save(commit = False)
+
+        post.title = post.title.capitalize()
+
+        if commit:
+            post.save()
+
+        return post
+
+    # def clean_title(self):
+    #     title = self.cleaned_data.get("title")
+    #
+    #     if not title[0].isupper():
+    #         title = title.capitalize()
+    #     return title
+
 
 
 class PostAddForm(PostBaseForm):
@@ -25,12 +68,8 @@ class PostEditForm(PostBaseForm):
     pass
 
 
-class PostDeleteForm(PostBaseForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields:
-            self.fields[field].disabled = True
+class PostDeleteForm(PostBaseForm, DisableFieldsMixin):
+    disabled_fields = ("__all__",)
 
 
 class SearchForm(forms.Form):
