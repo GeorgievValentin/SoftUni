77ee30ee8786d6d12b067edce1f1095d612abc4c
GeorgievValentin <ddarklizard@protonmail.com>
2024-10-17 18:29:47 +0300

    fix: remake some FBV to CBV

--- python_web/django_basics/projects/forumApp/forumApp/posts/urls.py
+++ python_web/django_basics/projects/forumApp/forumApp/posts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 
 from forumApp.posts import views
+from forumApp.posts.views import IndexView, PostAddView, DashboardView, PostEditView, PostDeleteView
 
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path("dashboard/", views.dashboard, name = "dashboard"),
-    path("add-post/", views.add_post, name = "add-post"),
+    path("", IndexView.as_view(), name = "index"),
+    path("dashboard/", DashboardView.as_view(), name = "dashboard"),
+    path("add-post/", PostAddView.as_view(), name = "add-post"),
     path("<int:pk>/", include([
-        path("edit-post/", views.edit_post, name = "edit-post"),
+        path("edit-post/", PostEditView.as_view(), name = "edit-post"),
         path("details-post/", views.details_post, name = "details-post"),
-        path("delete-post/", views.delete_post, name = "delete-post"),
+        path("delete-post/", PostDeleteView.as_view(), name = "delete-post"),
     ])),
 ]
--- python_web/django_basics/projects/forumApp/forumApp/posts/views.py
+++ python_web/django_basics/projects/forumApp/forumApp/posts/views.py
@@ -1,51 +1,115 @@
+from datetime import datetime
+
 from django.forms import modelform_factory
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import classonlymethod
+from django.views.generic import TemplateView, ListView, FormView, CreateView, UpdateView, DeleteView
+
 from forumApp.posts.forms import PostAddForm, PostDeleteForm, SearchForm, PostEditForm, PostCommentForm
 from forumApp.posts.models import PostModel, CommentModel
 
 
-def index(request):
-    post_form = modelform_factory(
-        PostModel,
-        fields = "__all__"
-    )
+class BaseView:
+    @classonlymethod
+    def as_view(cls):
 
-    context = {
-        "form": post_form,
-    }
+        def view(request, *args, **kwargs):
+            view_instance = cls()
+            return view_instance.dispatch(request, *args, **kwargs)
 
-    return render(request, "common/index.html", context)
+        return view
 
-
-def dashboard(request):
-    form = SearchForm(request.GET)
-    posts = PostModel.objects.all()
-
-    if request.method == "GET" and form.is_valid():
-        query = form.cleaned_data["query"]
-        posts = posts.filter(title__icontains = query)
-
-    context = {
-        "posts": posts,
-        "form": form,
-    }
-
-    return render(request, "posts/dashboard.html", context)
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == "GET":
+            return self.get(request, *args, **kwargs)
+        elif request.method == "POST":
+            return self.post(request, *args, **kwargs)
 
 
-def add_post(request):
-    form = PostAddForm(request.POST or None, request.FILES or None)
+class IndexView(TemplateView):
+    template_name = "common/index.html"   # static way
+    extra_context = {
+        "static_time": datetime.now(),
+    }  # static way
 
-    if request.method == "POST" and form.is_valid():
-        form.save()
+    def get_template_names(self):  # dynamic way
+        if self.request.user.is_authenticated:
+            return ["common/logged_index.html"]
+        else:
+            return ["common/index.html"]
 
-        return redirect("dashboard")
+    def get_context_data(self, **kwargs):  # dynamic way
+        contex = super().get_context_data(**kwargs)
 
-    context = {
-        "form": form,
-    }
+        contex["dynamic_time"] = datetime.now()
 
-    return render(request, "posts/add-post.html", context)
+        return contex
+
+# def index(request):
+#     post_form = modelform_factory(
+#         PostModel,
+#         fields = "__all__"
+#     )
+#
+#     context = {
+#         "form": post_form,
+#     }
+#
+#     return render(request, "common/index.html", context)
+
+
+class DashboardView(ListView, FormView):
+    template_name = "posts/dashboard.html"
+    context_object_name = "posts"
+    form_class = SearchForm
+    model = PostModel
+    success_url = reverse_lazy("dashboard")
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+
+        if queryset in self.request.GET:
+            query = self.request.GET.get("query")
+            queryset = self.queryset.filter(title__icontains = query)
+
+        return queryset
+
+# def dashboard(request):
+#     form = SearchForm(request.GET)
+#     posts = PostModel.objects.all()
+#
+#     if request.method == "GET" and form.is_valid():
+#         query = form.cleaned_data["query"]
+#         posts = posts.filter(title__icontains = query)
+#
+#     context = {
+#         "posts": posts,
+#         "form": form,
+#     }
+#
+#     return render(request, "posts/dashboard.html", context)
+
+
+class PostAddView(CreateView):
+    template_name = "posts/add-post.html"
+    model = PostModel
+    form_class = PostAddForm
+    success_url = reverse_lazy("dashboard")
+
+# def add_post(request):
+#     form = PostAddForm(request.POST or None, request.FILES or None)
+#
+#     if request.method == "POST" and form.is_valid():
+#         form.save()
+#
+#         return redirect("dashboard")
+#
+#     context = {
+#         "form": form,
+#     }
+#
+#     return render(request, "posts/add-post.html", context)
 
 
 def details_post(request, pk: int):
@@ -69,36 +133,60 @@ def details_post(request, pk: int):
     return render(request, "posts/details-post.html", context)
 
 
-def edit_post(request, pk: int):
-    post = PostModel.objects.get(pk = pk)
+class PostEditView(UpdateView):
+    template_name = "posts/edit-post.html"
+    model = PostModel
+    success_url = reverse_lazy("dashboard")
+    context_object_name = "post"
 
-    if request.method == "POST":
-        form = PostEditForm(request.POST, instance = post)
-        if form.is_valid():
-            form.save()
-            return redirect("dashboard")
-    else:
-        form = PostEditForm(instance = post)
+    def get_form_class(self):
+        if self.request.user.is_superuser:
+            return modelform_factory(PostModel, fields = "__all__")
+        else:
+            return modelform_factory(PostModel, fields = ("content",))
 
-    context = {
-        "form": form,
-        "post": post,
-    }
+# def edit_post(request, pk: int):
+#     post = PostModel.objects.get(pk = pk)
+#
+#     if request.method == "POST":
+#         form = PostEditForm(request.POST, instance = post)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("dashboard")
+#     else:
+#         form = PostEditForm(instance = post)
+#
+#     context = {
+#         "form": form,
+#         "post": post,
+#     }
+#
+#     return render(request, "posts/edit-post.html", context)
 
-    return render(request, "posts/edit-post.html", context)
 
+class PostDeleteView(DeleteView, FormView):
+    template_name = "posts/delete-post.html"
+    model = PostModel
+    form_class = PostDeleteForm
+    success_url = reverse_lazy("dashboard")
+    context_object_name = "post"
 
-def delete_post(request, pk: int):
-    post = PostModel.objects.get(pk = pk)
-    form = PostDeleteForm(instance = post)
+    def get_initial(self):
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        post = PostModel.objects.get(pk = pk)
+        return post.__dict__
 
-    if request.method == "POST":
-        post.delete()
-        return redirect("dashboard")
-
-    context = {
-        "form": form,
-        "post": post,
-    }
-
-    return render(request, "posts/delete-post.html", context)
+# def delete_post(request, pk: int):
+#     post = PostModel.objects.get(pk = pk)
+#     form = PostDeleteForm(instance = post)
+#
+#     if request.method == "POST":
+#         post.delete()
+#         return redirect("dashboard")
+#
+#     context = {
+#         "form": form,
+#         "post": post,
+#     }
+#
+#     return render(request, "posts/delete-post.html", context)
